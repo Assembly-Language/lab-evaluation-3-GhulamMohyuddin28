e8b782382c5113310f8d7780d1931083eedd2de6
GhulamMohyuddin28 <ghulammohyuddin.coder@gmail.com>
2024-12-30 12:27:46 +0500

    Second Last Commit

--- asm-file.asm
+++ asm-file.asm
@@ -1,27 +1,31 @@
-
-
 INCLUDE Irvine32.inc
 .data
-;public asmfunc
 msg db "assebly procedure end now",0
-
+sum DWORD 0
 
 .code
-asmfunc PROC p1:DWORD, p2:DWORD
+SumNegativeNumber PROC ;p1:DWORD, p2:DWORD
     
-    push ebp
-    mov ebp,esp
-    mov eax, [ebp+12
-\] ; Move the first parameter into EAX
-    call writedec
-    call crlf
-    ;add eax, p2 ; Add the second parameter to EAX
-    call writedec
-    call crlf
-    mov edx, offset msg
-    call writestring
-    call crlf
+    mov esi,ecx
+    mov ecx, edx
+    mov eax, 0
+
+    Sum_Loop:
+    CMP ecx, 0
+    JE end_Loop
     
+    mov ebx, [esi]
+    CMP ebx, 0
+    JGE skip_add
+
+    add eax, ebx
+    skip_add:
+    add esi, 4
+    sub ecx,1
+    JMP Sum_Loop
+
+    end_Loop:
     ret
-asmfunc ENDP
+    SumNegativeNumber ENDP
+
 end
--- c-file.c
+++ c-file.c
@@ -1,35 +1,31 @@
-
 #include <stdio.h>
-
-
 //extern "C" void __stdcall asmfunc(void);
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void __stdcall asmfunc(int p1 ,int p2);
+int __stdcall SumNegativeNumber(p1,p2);
 
 #ifdef __cplusplus
 }
 #endif
 
-
 int main() {
     system("cls");
-    int abc;
-    printf("assembly proc calling from  from C! \n");
-getch();
+    int array[10]={2,-3,4,-5,-6,7,8,-9, 1,-10};
+    printf("The array is: ");
+    for(int i = 0;  i<=10; i++)
+    {
+        printf("%d", array[i]);
+        printf(" ");
+    }
+    getch();
+    int size=sizeof(array)/sizeof(array[0]);
+    int sum = SumNegativeNumber(array,size);
+    printf("Sum of negative Numbers: %d\n", sum);
+    return 0;
 
-    
-    asmfunc(3,5); //assembly proc calling
-   
-   getch();
-    
-    
-    printf("back to  C! \n"); // printing in c
-    
-  
     
     return 0;
 }
